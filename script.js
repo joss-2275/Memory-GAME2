d468879d973c0280f87b0100efe6b28c92e0b9d6
joss-2275 <joss.wheatley@gmail.com>
2021-04-04 22:11:23 +0000

    the java for the memory game is completed, additional things to be added including more styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
+    let game = new MatchCards(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
@@ -17,49 +17,110 @@ function ready() {
     });
 }
 
-class MatchedCards {
+class MatchCards {
     constructor(totalTime, cards) {
         this.cardsArray = cards;
         this.totalTime = totalTime;
         this.timeRemaining = totalTime;
         this.timer = document.getElementById('time-remaining')
-        this.ticker = document.getElementById('flips-made');
+        this.ticker = document.getElementById('flips');
     }
-    startGame() {
+     startGame() {
         this.cardToCheck = null;
         this.totalClicks = 0;
         this.timeRemaining = this.totalTime;
-        this.MatchedCards = [];
+        this.matchedCards = [];
         this.busy = true;
+        setTimeout(() => {
+            this.shuffleCards(this.cardsArray);
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        }, 500)
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        this.ticker.innerText = this.totalClicks;
+       
     }
-    flipCard(card) {
 
+     hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            
+        });
+    }
+
+     shuffleCards(card) {
+        const cards = document.querySelectorAll(".card");
+        cards.forEach((card) => {
+            let randNumber = Math.floor(Math.random() * 16);
+            card.style.order = randNumber;
+        });
+    }
+
+     startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+
+     gameOver() {
+        clearInterval(this.countDown);
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+
+      victory() {
+        clearInterval(this.countDown);
+        document.getElementById('victory-text').classList.add('visible');
+    }
+
+     flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
+
+            if(this.cardToCheck) {
+                this.checkForCardMatch(card);
+            } else {
+                this.cardToCheck = card;
+            }
+        }
+    }
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else 
+            this.cardMismatch(card, this.cardToCheck);
+
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
+    }
+
+    getCardType(card) {
+        return card.getElementsByClassName('match-card')[0].src;
     }
     canFlipCard(card) {
-        return true;
-        //return (!this.busy && !this.MatchedCards.includes(card) && card !== this.cardToCheck)
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
+
 }
 
-
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-    //let game = new MixOrMatch(100, cards);
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
-}
 
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', ready());
