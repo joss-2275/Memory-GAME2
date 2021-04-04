e85885db7effddf074fa3e32a1a3d1e3941a1bd6
joss-2275 <joss.wheatley@gmail.com>
2021-04-04 17:12:57 +0000

    not much added, just needed to save

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);
+        });
+    });
 }
 
 class MatchedCards {
@@ -12,21 +25,44 @@ class MatchedCards {
         this.timer = document.getElementById('time-remaining')
         this.ticker = document.getElementById('flips-made');
     }
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.MatchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card) {
+
+    }
+    canFlipCard(card) {
+        return true;
+        //return (!this.busy && !this.MatchedCards.includes(card) && card !== this.cardToCheck)
+    }
 }
 
+
 function ready() {
-    let overlays = Array.from(document.querySelectorAll(".overlay-text"));
-    let cards = Array.from(document.querySelectorAll(".card"));
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    //let game = new MixOrMatch(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
-        overlay.classList.remove('visible');
-            //game.startGame();
+            overlay.classList.remove('visible');
+            game.startGame();
         });
     });
+
     cards.forEach(card => {
         card.addEventListener('click', () => {
-        //game.flipCard(card);
+            game.flipCard(card);
         });
     });
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
 }
